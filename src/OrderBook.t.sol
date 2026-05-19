512ffcba53177560346aa467c323ac7eec172e9b
kk <kedarmail@gmail.com>
2026-05-19 12:50:53 -0400

    basic tests with foundry for OrderBook.sol

---
+++ src/OrderBook.t.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "forge-std/Test.sol";
+import "../OrderBook.sol"; // modify according to your actual file structure
+import "src/MockERC20.sol"; // Mock ERC20 token for testing
+
+contract BankTest is Test {
+	OrderBook orderBook;
+	MockERC20 USDC;
+	MockERC20 EURT;
+	address owner = address(0x1);
+	address user1 = address(0x2);
+	address user2 = address(0x3);
+
+	function setUp() public {
+		USDC = new MockERC20("USDC", "USDC", 6);
+		EURT = new MockERC20("EURT", "EURT", 6);
+		orderBook = new OrderBook();
+		USDC.mint(user1, 10000e6);
+		vm.prank(user1);
+		USDC.approve(address(orderBook), 100e18);
+	}
+
+	function testPlaceOrderBeforeMarketCreation() public {
+		vm.expectRevert("createMarket before placing an order on it"); 
+		vm.prank(user1);
+		orderBook.placeOrder(address(USDC), address(EURT), OrderBook.Side.SELL, 100 * 1e18, 100 * 1e18);
+	}
+
+	function testCreateMarket() public {
+		orderBook.createMarket(address(USDC), address(EURT));
+	}
+	
+	function testPlaceOrder() public {
+		orderBook.createMarket(address(USDC), address(EURT));
+		vm.prank(user1);
+		USDC.approve(address(orderBook), 100e18);
+		USDC.mint(user1, 1000*1e18);
+		vm.prank(user1);
+		orderBook.placeOrder(address(USDC), address(EURT), OrderBook.Side.SELL, 100 * 1e18, 100 * 1e18);
+	}
+	
+}
